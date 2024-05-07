ed1c2c7b246aa8240afa2ae1748d3b40b8096e9a
Aline <eyramaline@gmail.com>
2024-05-07 11:00:30 +0000

    evolution projet laravel

--- NoteUniv/routes/web.php
+++ NoteUniv/routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,14 +20,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/*Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
+
+
+route::get('/home',[HomeController::class,'index'])->middleware('auth')->name('home');
+
+route::get('/post',[HomeController::class,'post'])->middleware(['auth', 'admin']);
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/accueil', [WebsiteController::class, 'accueil'])->name('accueil');
+
 });
 
 require __DIR__.'/auth.php';
